d007d94b4dded19aead791ffbc5470942bbfa769
Johnjimy Som <johnjimy.som@student.csulb.edu>
2015-10-02 06:48:29 +0900

    Create lab3_p2.c

---
+++ lab3/lab3_p2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+ * Stored in lab1 folder
+ */
+void main(int argc, char *argv[]) {
+	if (argc > 1) {
+		execlp("home/Documents/lab1", "lab1", argv[1], (char *) NULL);
+		perror ("exec failure");
+		exit(1);
+	}//end if
+	printf("\n Usage: %s text_file\n", *argv); 
+	exit(1);
