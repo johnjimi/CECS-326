63ae5818e05dfec300bb2ab14ffd25deef83250d
Johnjimy Som <johnjimy.som@student.csulb.edu>
2015-10-02 06:45:41 +0900

    Create lab3.c

---
+++ lab3/lab3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void main(int argc, char *argv[]) {
+	static char *new_argv[] = {"lab1", "5", (char *) 0};
+	execvp("/home/Documents/lab1", new_argv);
+	perror("exec failure");
+	exit(1);
+}//end main
